3851f8e68592756a1ad5ad1816f26791b94a2782
Christer Schallengruber <schallengruber@gmail.com>
2026-06-30 11:43:05 +0200

    Update dockutil-default-settings.sh
    Added no-restarta and Self Service+

--- dockutil-default-settings.sh
+++ dockutil-default-settings.sh
@@ -4,13 +4,20 @@
 dockutil --remove all
 
 #Set default dock icons
-dockutil --add /Applications/Microsoft\ Word.app
-dockutil --add /Applications/Microsoft\ To\ Do.app
-dockutil --add /Applications/Microsoft\ Teams.app
-dockutil --add /Applications/Microsoft\ PowerPoint.app
-dockutil --add /Applications/Microsoft\ Outlook.app
-dockutil --add /Applications/Microsoft\ OneNote.app
-dockutil --add /Applications/Microsoft\ Excel.app
-dockutil --add /Applications/Microsoft\ 365\ Copilot.app
+dockutil --add --no-restart /Applications/Microsoft\ Word.app
+dockutil --add --no-restart /Applications/Microsoft\ Teams.app
+dockutil --add --no-restart /Applications/Microsoft\ PowerPoint.app
+dockutil --add --no-restart /Applications/Microsoft\ Outlook.app
+dockutil --add --no-restart /Applications/Microsoft\ OneNote.app
+dockutil --add --no-restart /Applications/Microsoft\ Excel.app
+
+dockutil --add /Applications/Self\ Service+.app
+
+
+## Microsoft To Do.app will be added in a later stage if applicable.
+## dockutil --add --no-restart /Applications/Microsoft\ To\ Do.app
+## dockutil --add --no-restart /Applications/Microsoft\ 365\ Copilot.app
+
+
 
 exit 0
