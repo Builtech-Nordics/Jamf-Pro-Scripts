abd88942d56f3d6447589ea979167215363e7a69
Christer Schallengruber <schallengruber@gmail.com>
2026-06-30 10:57:52 +0200

    Create dockutil-default-settings.sh
    Init

---
+++ dockutil-default-settings.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Clean dock
+dockutil --remove all
+
+#Set default dock icons
+dockutil --add /Applications/Microsoft\ Word.app
+dockutil --add /Applications/Microsoft\ To\ Do.app
+dockutil --add /Applications/Microsoft\ Teams.app
+dockutil --add /Applications/Microsoft\ PowerPoint.app
+dockutil --add /Applications/Microsoft\ Outlook.app
+dockutil --add /Applications/Microsoft\ OneNote.app
+dockutil --add /Applications/Microsoft\ Excel.app
+dockutil --add /Applications/Microsoft\ 365\ Copilot.app
+
+exit 0
