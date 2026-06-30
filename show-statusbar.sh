24d76d333b68b31849bb57f241da958728402717
Christer Schallengruber <schallengruber@gmail.com>
2026-06-30 15:10:27 +0200

    Init statusbar

---
+++ show-statusbar.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+user=$(stat -f%Su /dev/console)
+uid=$(id -u "$user" 2>/dev/null)
+
+[ -z "$user" ] || [ "$user" = "root" ] && exit 0
+
+launchctl asuser "$uid" sudo -u "$user" defaults write com.apple.finder ShowStatusBar -bool true
+launchctl asuser "$uid" sudo -u "$user" killall Finder
+
+exit 0
