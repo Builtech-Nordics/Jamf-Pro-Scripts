393059079afdc502be84adb7aa4bd5975b52085d
Christer Schallengruber <schallengruber@gmail.com>
2026-06-30 11:55:47 +0200

    Update dockutil-default-settings.sh
    Test

--- dockutil-default-settings.sh
+++ dockutil-default-settings.sh
@@ -4,19 +4,19 @@
 dockutil --remove all
 
 # Set default dock icons
-dockutil --no-restart --add /Applications/Microsoft\ Word.app
-dockutil --no-restart --add /Applications/Microsoft\ Teams.app
-dockutil --no-restart --add /Applications/Microsoft\ PowerPoint.app
-dockutil --no-restart --add /Applications/Microsoft\ Outlook.app
-dockutil --no-restart --add /Applications/Microsoft\ OneNote.app
-dockutil --no-restart --add /Applications/Microsoft\ Excel.app
+dockutil --add /Applications/Microsoft\ Word.app
+dockutil --add /Applications/Microsoft\ Teams.app
+dockutil --add /Applications/Microsoft\ PowerPoint.app
+dockutil --add /Applications/Microsoft\ Outlook.app
+dockutil --add /Applications/Microsoft\ OneNote.app
+dockutil --add /Applications/Microsoft\ Excel.app
 
 dockutil --add /Applications/Self\ Service+.app
 
 
 # Microsoft To Do.app will be added in a later stage if applicable.
-# dockutil --add --no-restart /Applications/Microsoft\ To\ Do.app
-# dockutil --add --no-restart /Applications/Microsoft\ 365\ Copilot.app
+# dockutil --add /Applications/Microsoft\ To\ Do.app
+# dockutil --add /Applications/Microsoft\ 365\ Copilot.app
 
 
 
