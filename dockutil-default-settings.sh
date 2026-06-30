47e27049b5af38a0fc929549c1429357debd7ec2
Christer Schallengruber <schallengruber@gmail.com>
2026-06-30 11:49:55 +0200

    Update dockutil-default-settings.sh
    Adjustments, place  --no-restart before path

--- dockutil-default-settings.sh
+++ dockutil-default-settings.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-#Clean dock
+# Clean dock
 dockutil --remove all
 
-#Set default dock icons
-dockutil --add --no-restart /Applications/Microsoft\ Word.app
-dockutil --add --no-restart /Applications/Microsoft\ Teams.app
-dockutil --add --no-restart /Applications/Microsoft\ PowerPoint.app
-dockutil --add --no-restart /Applications/Microsoft\ Outlook.app
-dockutil --add --no-restart /Applications/Microsoft\ OneNote.app
-dockutil --add --no-restart /Applications/Microsoft\ Excel.app
+# Set default dock icons
+dockutil --no-restart --add /Applications/Microsoft\ Word.app
+dockutil --no-restart --add /Applications/Microsoft\ Teams.app
+dockutil --no-restart --add /Applications/Microsoft\ PowerPoint.app
+dockutil --no-restart --add /Applications/Microsoft\ Outlook.app
+dockutil --no-restart --add /Applications/Microsoft\ OneNote.app
+dockutil --no-restart --add /Applications/Microsoft\ Excel.app
 
 dockutil --add /Applications/Self\ Service+.app
 
 
-## Microsoft To Do.app will be added in a later stage if applicable.
-## dockutil --add --no-restart /Applications/Microsoft\ To\ Do.app
-## dockutil --add --no-restart /Applications/Microsoft\ 365\ Copilot.app
+# Microsoft To Do.app will be added in a later stage if applicable.
+# dockutil --add --no-restart /Applications/Microsoft\ To\ Do.app
+# dockutil --add --no-restart /Applications/Microsoft\ 365\ Copilot.app
 
 
 
