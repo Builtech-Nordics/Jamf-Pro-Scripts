54c6f7f5fae0073f34764eec7e8828b1cbb111c5
Christer Schallengruber <schallengruber@gmail.com>
2026-06-30 12:18:45 +0200

    Changes

--- desktoppr-default-settings.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-desktoppr https://raw.githubusercontent.com/scriptingosx/desktoppr/profile/examples/BoringBlueDesktop.png
-
-
-exit 0
---
+++ desktoppr-default-settings/desktoppr-default-settings.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+SCRIPT_NAME="desktoppr-default-settings"
+
+desktoppr https://raw.githubusercontent.com/Builtech-Nordics/Jamf-Pro-Scripts/refs/heads/main/${SCRIPT_NAME}/backgroundImage.png
+
+exit 0
